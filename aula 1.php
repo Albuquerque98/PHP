96f2f29a634547a7bcf34a5ece985b56fc55e409
Albuquerque98 <100594430+Albuquerque98@users.noreply.github.com>
2024-05-31 10:45:20 -0300

    Add files via upload

---
+++ aula 1.php
@@ -0,0 +1,14 @@
+<?php
+    
+    //echo "Hello, world!"
+
+    // Definindo variáveis
+    $numero1 = 5;
+    $numero2 = 10;
+    $soma = 0;
+
+    //Fazendo a soma
+    $soma = $numero1 + $numero2;
+    echo "A soma é igual a $soma"
+
+?>
